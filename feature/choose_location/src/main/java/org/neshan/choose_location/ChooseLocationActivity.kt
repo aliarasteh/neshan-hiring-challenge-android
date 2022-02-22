eafcbdc7a49d86cc8652afdfdedc0eccb6d18881
Arasteh <ali.arasteh@mehrparsict.com>
2022-02-22 20:08:58 +0330

    implement choose location activity

--- feature/choose_location/src/main/java/org/neshan/choose_location/ChooseLocationActivity.kt
+++ feature/choose_location/src/main/java/org/neshan/choose_location/ChooseLocationActivity.kt
@@ -1,16 +1,62 @@
 package org.neshan.choose_location
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.IntentSender.SendIntentException
+import android.graphics.BitmapFactory
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.carto.styles.MarkerStyleBuilder
+import com.carto.utils.BitmapUtils
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import dagger.hilt.android.AndroidEntryPoint
 import org.neshan.choose_location.databinding.ActivityChooseLocationBinding
+import org.neshan.common.model.LatLng
+import org.neshan.mapsdk.model.Marker
+import java.text.DateFormat
+import java.util.*
 
+@AndroidEntryPoint
 class ChooseLocationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChooseLocationBinding
 
+    // User's current location
+    private var userLocation: Location? = null
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var settingsClient: SettingsClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var locationSettingsRequest: LocationSettingsRequest? = null
+    private var locationCallback: LocationCallback? = null
+    private var lastUpdateTime: String? = null
+
+    // boolean flag to toggle the ui
+    private var mRequestingLocationUpdates: Boolean? = null
+    private var marker: Marker? = null
+
     companion object {
+        private const val TAG = "ChooseLocationActivity"
+
         const val KEY_LATITUDE = "latitude"
         const val KEY_LONGITUDE = "longitude"
+
+        // used to track request permissions
+        private const val REQUEST_CODE = 123
+        private const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 1000
+        private const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS: Long = 1000
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +64,238 @@ class ChooseLocationActivity : AppCompatActivity() {
         binding = ActivityChooseLocationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.submit.setOnClickListener {
-            setResult(RESULT_OK, Intent().apply {
-                putExtra(KEY_LATITUDE, 35.667057)
-                putExtra(KEY_LONGITUDE, 51.508333)
-            })
-            finish()
+        initViews()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initMap()
+
+        // Initializing user location
+        initLocation()
+        startReceivingLocationUpdates()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    private fun initViews() {
+        binding.back.setOnClickListener {
+            onBackPressed()
         }
+
+        binding.location.setOnClickListener {
+            focusOnUserLocation()
+        }
+
+        binding.confirm.setOnClickListener {
+            chooseSelectedPosition()
+        }
+    }
+
+    private fun initMap() {
+        // Setting map focal position to a fixed position and setting camera zoom
+//        binding.mapview.moveCamera(LatLng(35.767234, 51.330743), 0f)
+        binding.mapview.setZoom(14f, 0f)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+
+    private fun initLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        settingsClient = LocationServices.getSettingsClient(this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                // location is received
+                userLocation = locationResult.lastLocation
+                lastUpdateTime = DateFormat.getTimeInstance().format(Date())
+                onLocationChange()
+            }
+        }
+        mRequestingLocationUpdates = false
+        locationRequest = LocationRequest()
+        locationRequest!!.interval = UPDATE_INTERVAL_IN_MILLISECONDS
+        locationRequest!!.fastestInterval = FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
+        locationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationRequest!!)
+        locationSettingsRequest = builder.build()
+    }
+
+
+    /**
+     * Starting location updates
+     * Check whether location settings are satisfied and then
+     * location updates will be requested
+     */
+    // todo check permission
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        settingsClient?.checkLocationSettings(locationSettingsRequest!!)
+            ?.addOnSuccessListener(this) {
+                Log.i(TAG, "All location settings are satisfied.")
+
+                fusedLocationClient?.requestLocationUpdates(
+                    locationRequest!!,
+                    locationCallback!!,
+                    Looper.myLooper()!!
+                )
+                onLocationChange()
+            }
+            ?.addOnFailureListener(this) { e ->
+                val statusCode = (e as ApiException).statusCode
+                when (statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                        Log.i(
+                            TAG,
+                            "Location settings are not satisfied. Attempting to upgrade location settings "
+                        )
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the
+                            // result in onActivityResult().
+                            val rae = e as ResolvableApiException
+                            rae.startResolutionForResult(this@ChooseLocationActivity, REQUEST_CODE)
+                        } catch (sie: SendIntentException) {
+                            Log.i(
+                                TAG,
+                                "PendingIntent unable to execute request."
+                            )
+                        }
+                    }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                        val errorMessage = "Location settings are inadequate, and cannot be " +
+                                "fixed here. Fix in Settings."
+                        Log.e(TAG, errorMessage)
+//                        Toast.makeText(this@ChooseLocationActivity, errorMessage, Toast.LENGTH_LONG)
+//                            .show()
+                    }
+                }
+                onLocationChange()
+            }
+    }
+
+    private fun stopLocationUpdates() {
+        // Removing location updates
+        fusedLocationClient
+            ?.removeLocationUpdates(locationCallback!!)
+            ?.addOnCompleteListener(
+                this
+            ) {
+//                Toast.makeText(applicationContext, "Location updates stopped!", Toast.LENGTH_SHORT)
+//                    .show()
+            }
+    }
+
+    fun startReceivingLocationUpdates() {
+        // Requesting ACCESS_FINE_LOCATION using Dexter library
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                    mRequestingLocationUpdates = true
+                    startLocationUpdates()
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    if (response.isPermanentlyDenied) {
+                        // open device settings when the permission is
+                        // denied permanently
+                        openSettings()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest, token: PermissionToken
+                ) {
+                    token.continuePermissionRequest()
+                }
+
+            }).check()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE -> when (resultCode) {
+                RESULT_OK -> Log.e(
+                    TAG,
+                    "User agreed to make required location settings changes."
+                )
+                RESULT_CANCELED -> {
+                    Log.e(
+                        TAG,
+                        "User chose not to make required location settings changes."
+                    )
+                    mRequestingLocationUpdates = false
+                }
+            }
+        }
+    }
+
+    private fun openSettings() {
+        val intent = Intent()
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        val uri = Uri.fromParts("package", application.packageName, null)
+        intent.data = uri
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+
+    private fun onLocationChange() {
+        if (userLocation != null) {
+            addUserMarker(LatLng(userLocation!!.latitude, userLocation!!.longitude))
+        }
+    }
+
+
+    // This method gets a LatLng as input and adds a marker on that position
+    private fun addUserMarker(loc: LatLng) {
+        //remove existing marker from map
+        if (marker != null) {
+            binding.mapview.removeMarker(marker)
+        }
+        // Creating marker style. We should use an object of type MarkerStyleCreator, set all features on it
+        // and then call buildStyle method on it. This method returns an object of type MarkerStyle
+        val markStCr = MarkerStyleBuilder()
+        markStCr.size = 30f
+        markStCr.bitmap = BitmapUtils.createBitmapFromAndroidBitmap(
+            BitmapFactory.decodeResource(
+                resources, R.drawable.ic_marker
+            )
+        )
+        val markSt = markStCr.buildStyle()
+
+        // Creating user marker
+        marker = Marker(loc, markSt)
+
+        // Adding user marker to map!
+        binding.mapview.addMarker(marker)
+    }
+
+    private fun focusOnUserLocation() {
+        if (userLocation != null) {
+            binding.mapview.moveCamera(
+                LatLng(userLocation!!.latitude, userLocation!!.longitude), 0.25f
+            )
+            binding.mapview.setZoom(15f, 0.25f)
+        }
+    }
+
+    private fun chooseSelectedPosition() {
+        val latLng = binding.mapview.cameraTargetPosition
+        setResult(RESULT_OK, Intent().apply {
+            putExtra(KEY_LATITUDE, latLng.latitude)
+            putExtra(KEY_LONGITUDE, latLng.longitude)
+        })
+        onBackPressed()
     }
 }
